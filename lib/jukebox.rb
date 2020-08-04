c652f19c8f89ea55e4f32bd4e622c3a1e6a7ca18
Chandler Hanson <chan.hanson5@gmail.com>
2020-08-04 01:47:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,56 @@
-# Add your code here
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each.with_index(1) do |song, i|
+    puts "#{i}. #{song}"
+  end
+end
+
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_song = gets.strip
+  found_song = ""
+  songs.each do |song|
+    if song.include? user_song
+      found_song = user_song
+    end
+  end
+end
+
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  puts "Please enter a command:"
+  user_input = gets.strip
+  if user_input == "help"
+    help
+  elsif user_input == "list"
+    list(songs)
+  elsif user_input == "play"
+    play(songs)
+  elsif user_input == "exit"
+    exit_jukebox
+  end
+end
